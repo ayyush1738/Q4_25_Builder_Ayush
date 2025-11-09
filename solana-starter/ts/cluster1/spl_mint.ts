64ec8d470b828c0ec1af3a6b620a09c6c4a3cd50
unknown <singhrathoreayush824@gmail.com>
2025-11-09 22:18:02 +0530

    starter

---
+++ solana-starter/ts/cluster1/spl_mint.ts
@@ -0,0 +1,29 @@
+import { Keypair, PublicKey, Connection, Commitment } from "@solana/web3.js";
+import { getOrCreateAssociatedTokenAccount, mintTo } from '@solana/spl-token';
+import wallet from "./wallet/wallet.json"
+
+// Import our keypair from the wallet file
+const keypair = Keypair.fromSecretKey(new Uint8Array(wallet));
+
+//Create a Solana devnet connection
+const commitment: Commitment = "confirmed";
+const connection = new Connection("https://api.devnet.solana.com", commitment);
+
+const token_decimals = 1_000_000n;
+
+// Mint address
+const mint = new PublicKey("9DqC3ZGUP847tUE83awF2WeTyw7wM93WtEojnHDKJR2T");
+
+(async () => {
+    try {
+        // Create an ATA
+        const ata = getOrCreateAssociatedTokenAccount(connection, keypair, mint, keypair.publicKey);
+        console.log(`Your ata is: ${(await ata).address.toBase58()}`);
+
+        // Mint to ATA
+        const mintTx = await mintTo(connection, keypair, mint, (await ata).address, keypair.publicKey, 10000)
+        console.log(`Your mint txid: ${mintTx}`);
+    } catch(error) {
+        console.log(`Oops, something went wrong: ${error}`)
+    }
+})()
